b89d4f6452912aa614c77edce76dc2ad2709e1af
Prashant Pandey <prashants_pandey@outlook.com>
2017-08-01 03:23:15 +0530

    changed static address

--- bodhiai/settings.py
+++ bodhiai/settings.py
@@ -132,4 +132,4 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'basicinformation/static/'
