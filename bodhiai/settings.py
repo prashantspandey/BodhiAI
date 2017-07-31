79beb884c8ba61a022c7583644d620a9445830d2
Prashant Pandey <prashants_pandey@outlook.com>
2017-08-01 03:25:28 +0530

    again static address

--- bodhiai/settings.py
+++ bodhiai/settings.py
@@ -131,5 +131,5 @@ USE_TZ = True
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'basicinformation/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'/basicinformation/static')
+STATIC_URL = '/basicinformation/static/'
