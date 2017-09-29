95d43efd148aef7642eaebe6a028102ae48ea697
Prashant Pandey <prashants_pandey@outlook.com>
2017-09-29 23:00:08 +0530

    changed some graph sizes

--- bodhiai/views.py
+++ bodhiai/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     context = {'hello':'hello'}
-    return render(request,'basicinformation/bodhiHomePage1.html',context)
+    return render(request,'basicinformation/bodhiHomePage.html',context)
