ef9a75a8f4f0e31946695df5bb8fbb4224e33da8
Prashant Pandey <prashants_pandey@outlook.com>
2018-01-30 17:01:21 +0530

    added groupx quests

--- basicinformation/views.py
+++ basicinformation/views.py
@@ -46,7 +46,8 @@ def home(request):
             #add_questions('Govindam Defence Academy')
             #sheet_links = ['groupx03math.csv','groupx03physics.csv']
             #sheet_links = ['groupx04math.csv','groupx04physics.csv']
-            sheet_links = ['groupx05math.csv','groupx05physics.csv']
+            sheet_links =\
+            ['groupx03math.csv','groupx03physics.csv''groupx04math.csv','groupx04physics.csv','groupx05math.csv','groupx05physics.csv']
             #add_to_database_questions(sheet_links,extra_info = True,onlyImage = True)
             add_to_database_questions(sheet_links,'Govindam Defence Academy',extra_info =
                                       True,onlyImage=True)
