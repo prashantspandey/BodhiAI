f2c2dd091e2e6d6eb505480884a599784bd3f9c2
Prashant Pandey <prashants_pandey@outlook.com>
2018-02-15 15:46:01 +0530

    wrong file name

--- basicinformation/views.py
+++ basicinformation/views.py
@@ -100,7 +100,7 @@ def home(request):
 
 
             #add_teachers('teachers.csv','Govindam Defence Academy',production=True)
-            add_students('4th2semjecrc.csv','JECRC',production=True)
+            add_students('4thsem2jecrc.csv','JECRC',production=True)
             #add_students('students3.csv','Govindam Defence Academy',production=True)
             #add_questions('Colonel Defence Academy','Defence-Physics')
             #add_questions('Colonel Defence Academy','Defence-English')
