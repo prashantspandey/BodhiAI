0862f29d770e1ebbfdc654e55277fae383be68bd
Prashant Pandey <prashants_pandey@outlook.com>
2018-08-18 01:45:33 +0530

    added choices to questions api

--- QuestionsAndPapers/api/serializers.py
+++ QuestionsAndPapers/api/serializers.py
@@ -9,10 +9,24 @@ class SchoolDisplaySerializer(serializers.ModelSerializer):
             'name',
         ]
 
+class ChoicesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choices
+        fields = [
+            'text',
+            'picture',
+            'explanation',
+            'explanationPicture',
+            'predicament',
+
+        ]
+
 
 class SSCQuestionSerializer(serializers.ModelSerializer):
+    choices = serializers.SerializerMethodField() 
     class Meta:
         model = SSCquestions
+        depth = 1
         fields = [
             'comprehension',
             'max_marks',
@@ -22,6 +36,11 @@ class SSCQuestionSerializer(serializers.ModelSerializer):
             'picture',
             'source',
             'language',
+            'choices',
 
         ]
+
+    def get_choices(self,obj):
+        return\
+    ChoicesSerializer(obj.choices_set.all(),many=True,read_only=True).data
 
