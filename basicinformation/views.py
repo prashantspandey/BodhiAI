cf3ce5b01858926fcf5026092e632b5780a99bc1
Prashant Pandey <prashants_pandey@outlook.com>
2018-08-18 13:56:11 +0530

    add locopilot questions

--- basicinformation/views.py
+++ basicinformation/views.py
@@ -303,9 +303,9 @@ def home(request):
             #sheet_links2 = \
             #        ['statement_ry.csv']
             #adding_quest =\
-            delete_allQuestions.delay("JEN")
+            #delete_allQuestions.delay("JEN")
             #allquestions_institute.delay('Quantitative-Analysis',"JEN")
-            #add_to_database_questions.delay(sheet_links,'JEN',production=True,onlyImage=True)
+            add_to_database_questions.delay(sheet_links,'JEN',production=True,onlyImage=True)
             #add_questions('JEN','ElectricalLocoPilot')
             #quest_added = add_to_database_questions.delay(sheet_links,'Swami Reasoning World',onlyImage=True,production =\
             #                          True)
