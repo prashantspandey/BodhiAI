f131e5f51a02978059b7ec4c53ccec9430779f32
Prashant Pandey <prashants_pandey@outlook.com>
2018-09-22 00:12:53 +0530

    debug

--- Recommendations/models.py
+++ Recommendations/models.py
@@ -29,3 +29,15 @@ class RecommendedContent(models.Model):
 #
 #    def __str__(self):
 #        return str(self.subject) +' '+ str(self.chapter) 
+
+class Concepts(models.Model):
+    question = models.ForeignKey(SSCquestions,null=True,blank=True)
+    name = models.CharField(max_length=200)
+    content = models.ForeignKey(RecommendedContent,null=True,blank=True)
+    subject = models.CharField(max_length =200)
+    chapter = models.FloatField(blank=True,null=True)
+    concept_number = models.IntegerField(blank=True,null=True)
+
+
+    def __str__(self):
+        return str(self.subject) +' '+ str(self.chapter) 
