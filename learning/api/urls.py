f2c90da017ffbc0f59d423cda6a43bedc749c84c
Prashant Pandey <prashants_pandey@outlook.com>
2018-09-25 14:54:07 +0530

    debug

--- learning/api/urls.py
+++ learning/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from learning.api import views
 
 urlpatterns = [
-    url(r'learning_subjects/$',views.StudentListAPIView.as_view(),name='LearningSubjects'),
+    url(r'learning_subjects/$',views.StudentSubjectsAPIView.as_view(),name='LearningSubjects'),
     url(r'learning_chapters/$',views.StudentGetChaptersAPIView.as_view(),name='GetChapters'),
 ]
