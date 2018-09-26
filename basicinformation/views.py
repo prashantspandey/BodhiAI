b1b26a8b7511450f8d97c82b7af2f5b0add50149
Prashant Pandey <prashants_pandey@outlook.com>
2018-09-27 01:01:34 +0530

    debug

--- basicinformation/views.py
+++ basicinformation/views.py
@@ -326,7 +326,7 @@ def home(request):
             #sheet_links =\
             #['age.csv','alligations.csv','average.csv','boat_and_stream.csv','discount.csv','fraction.csv','lcm_lcf.csv','number_system.csv','percentage.csv','pipe_cistern.csv','ratio_proportions.csv','simple_compound_interest.csv','simplification.csv','speed_distance.csv','square_cube_roots.csv','surds.csv','time_work.csv','train.csv','volume.csv',]
             sheet_links = \
-                    ['13_5.csv','13_6.csv','13_7.csv']
+                    ['13_7.csv']
 
             #start_caching_prgress.delay()
             #delete_timing_cache.delay()
