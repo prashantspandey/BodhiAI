616fdf03492fbf68501305666b020207449ecc27
Prashant Pandey <prashants_pandey@outlook.com>
2018-09-27 22:15:06 +0530

    debug

--- basicinformation/views.py
+++ basicinformation/views.py
@@ -327,18 +327,8 @@ def home(request):
             #['age.csv','alligations.csv','average.csv','boat_and_stream.csv','discount.csv','fraction.csv','lcm_lcf.csv','number_system.csv','percentage.csv','pipe_cistern.csv','ratio_proportions.csv','simple_compound_interest.csv','simplification.csv','speed_distance.csv','square_cube_roots.csv','surds.csv','time_work.csv','train.csv','volume.csv',]
             sheet_links = \
                     ['13_7.csv']
-            students = Student.objects.filter(school__name = "JEN")
-            print('{} total student'.format(len(students)))
-            for stud in students:
-                subjects = stud.subject_set.all()
-                print('{} student {} subjects '.format(stud,subjects))
-                for sub in subjects:
-                    chapters = get_chapters(sub.name)
-                    for chap in chapters:
-                        createProgressCache(stud.id,sub.name,chap)
 
-
-            #start_caching_prgress.delay()
+            start_caching_prgress.delay()
             #delete_timing_cache.delay()
             #cache = StudentAverageTimingDetailCache.objects.all()
             #print('{} len of cache'.format(cache))
