690806f368e30d891562019a3cf1a44b1f4920d9
Prashant Pandey <prashants_pandey@outlook.com>
2018-10-01 15:34:05 +0530

    debug

--- membership/api/views.py
+++ membership/api/views.py
@@ -216,9 +216,14 @@ class CustomLoginAPIView(APIView):
         token, created = Token.objects.get_or_create(user=user)
         groups = user.groups.all()
         deleteBadTests.delay()
-        student_details = StudentDetails.objects.get(student = user)
-        token_context  = \
+        try:
+            student_details = StudentDetails.objects.get(student = user)
+            token_context  = \
                 {'key':token.key,'user_type':groups[0].name,'name':user.username,'photo':student_details.photo}
+        except:
+            token_context  = \
+                {'key':token.key,'user_type':groups[0].name,'name':user.username,'photo':None}
+
         return Response(token_context,status = 200)
 
 class CustomLogoutAPIView(APIView):
