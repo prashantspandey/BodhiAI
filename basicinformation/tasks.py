410ebc7c63f5f357407dce44de6b90663427dc55
Prashant Pandey <prashants_pandey@outlook.com>
2018-10-18 21:02:43 +0530

    Debug

--- basicinformation/tasks.py
+++ basicinformation/tasks.py
@@ -18,7 +18,19 @@ import json
 from django.db.models.signals import post_save
 from notifications.api.views import *
 from membership.models import *
-#from Recommendations.api.views import youtube_search
+from apiclient.discovery import build 
+from apiclient.errors import HttpError 
+from oauth2client.tools import argparser 
+import pprint 
+
+
+
+DEVELOPER_KEY = "AIzaSyDOW6Nt-1jpzxcEbypSpJ-ObCsZHjYBjPA" 
+
+
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
 
 @shared_task
 def bring_teacher_subjects_analysis(user_id):
@@ -2696,6 +2708,72 @@ def notification_create_timetable(title,body,sender_id,batch):
     me = Teach(user)
     school = me.my_school()
     TimeTableNotification(title,body,school.name,batch)
+
+
+
+def\
+youtube_search(q,max_results=5,order='relevance',token=None,location=None,location_radius=None):
+    print('got here')
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=
+                    DEVELOPER_KEY)
+    search_response = youtube.search().list(
+        q = q,
+        type = "video",
+        pageToken = token,
+        order = order,
+        part = "id,snippet",
+        maxResults = max_results,
+        location = location,
+        locationRadius = location_radius).execute()
+
+    title = []
+    channelId = []
+    channelTitle = []
+    categoryId = []
+    videoId = []
+    viewCount = []
+    likeCount = []
+    dislikeCount = []
+    commentCount = []
+    favoriteCount = []
+    category = []
+    tags = []
+    videos = []
+
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            title.append(search_result['snippet']['title'])
+
+            videoId.append(search_result['id']['videoId'])
+
+            response = youtube.videos().list(
+                part='statistics, snippet',
+                id=search_result['id']['videoId']).execute()
+
+            channelId.append(response['items'][0]['snippet']['channelId'])
+            channelTitle.append(response['items'][0]['snippet']['channelTitle'])
+            categoryId.append(response['items'][0]['snippet']['categoryId'])
+            favoriteCount.append(response['items'][0]['statistics']['favoriteCount'])
+            viewCount.append(response['items'][0]['statistics']['viewCount'])
+            likeCount.append(response['items'][0]['statistics']['likeCount'])
+            dislikeCount.append(response['items'][0]['statistics']['dislikeCount'])
+
+        if 'commentCount' in response['items'][0]['statistics'].keys():
+            commentCount.append(response['items'][0]['statistics']['commentCount'])
+
+        else:
+            commentCount.append([])
+
+        if 'tags' in response['items'][0]['snippet'].keys():
+            tags.append(response['items'][0]['snippet']['tags'])
+        else:
+            tags.append([])
+
+    youtube_dict = {'tags':tags,'channelId': channelId,'channelTitle': channelTitle,'categoryId':categoryId,'title':title,'videoId':videoId,'viewCount':viewCount,'likeCount':likeCount,'dislikeCount':dislikeCount,'commentCount':commentCount,'favoriteCount':favoriteCount}
+
+    return youtube_dict
+
+
 
 
 @shared_task
