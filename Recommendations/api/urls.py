14370853cdf922be8d86567043546525ee19be30
Prashant Pandey <prashants_pandey@outlook.com>
2018-10-19 11:48:33 +0530

    debug

--- Recommendations/api/urls.py
+++ Recommendations/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from learning.api import views
+from ..api import views
 
 urlpatterns = [
     url(r'chapter_recommendations/$',views.StudentChapterYoutubeRecommendationsAPIView.as_view(),name='ChapterRecommendations'),
